407469133d8496176aad96fd061e27827040885e
Nur Fikri <codingki@gmail.com>
2022-01-21 19:35:19 +0700

    feat: easy share to twitter

--- components/StatsModal.tsx
+++ components/StatsModal.tsx
@@ -63,6 +63,38 @@ export default function StatsModal(props: Props) {
     }
   }
 
+  function handleShareToTwitter() {
+    const num = Math.ceil(
+      (new Date(date).getTime() - new Date("2022-01-20").getTime()) /
+        24 /
+        60 /
+        60 /
+        1000
+    );
+    const score =
+      gameState.answers[gameState.attempt - 1] === answer
+        ? gameState.attempt
+        : "X";
+    let text = `Katla ${num} ${score}/6%0A%0A`;
+
+    gameState.answers.filter(Boolean).forEach((userAnswer) => {
+      userAnswer.split("").forEach((char, i) => {
+        if (answer[i] === char) {
+          text += "🟩";
+        } else if (answer.includes(char)) {
+          text += "🟨";
+        } else {
+          text += "⬛";
+        }
+      });
+      text += "%0A";
+    });
+
+    text += "%0Ahttps://katla.vercel.app";
+    const shareToTwitter = `https://twitter.com/intent/tweet?text=${text}`;
+    window.open(shareToTwitter, "_blank");
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <Modal.Title>Statistik</Modal.Title>
@@ -134,23 +166,41 @@ export default function StatsModal(props: Props) {
             <RemainingTime />
           </div>
           <div className="bg-gray-400" style={{ width: 1 }}></div>
-          <button
-            onClick={handleShare}
-            className="bg-green-700 py-1 md:py-3 px-3 md:px-6 rounded-md font-semibold uppercase text-xl flex flex-1 flex-row gap-2 items-center justify-center"
-          >
-            <div>Share</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 0 24 24"
-              width="24"
+          <div className="flex flex-col gap-4">
+            <button
+              onClick={handleShare}
+              className="bg-green-700 py-1 md:py-3 px-3 md:px-6 rounded-md font-semibold uppercase text-xl flex flex-1 flex-row gap-2 items-center justify-center"
             >
-              <path
-                fill="currentColor"
-                d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92s2.92-1.31 2.92-2.92c0-1.61-1.31-2.92-2.92-2.92zM18 4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7.02c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"
-              ></path>
-            </svg>
-          </button>
+              <div>Share</div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92s2.92-1.31 2.92-2.92c0-1.61-1.31-2.92-2.92-2.92zM18 4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7.02c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"
+                ></path>
+              </svg>
+            </button>
+            <button
+              onClick={handleShareToTwitter}
+              className="bg-green-700 py-1 md:py-3 px-3 md:px-6 rounded-md font-semibold uppercase text-xl flex flex-1 flex-row gap-2 items-center justify-center"
+              style={{ backgroundColor: "#00acee" }}
+            >
+              <div>Share</div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="#ffffff"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+              </svg>
+            </button>
+          </div>
         </div>
       )}
     </Modal>
