18d61e0970e38e7f3af01a8de1e103adb7c5b9c7
Fatih Kalifa <fatihkalifa@gmail.com>
2022-01-28 22:04:41 +0700

    ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,16 @@ export default function MyApp({ Component, pageProps }) {
         src="https://cdn.metrical.xyz/script.js"
         strategy="afterInteractive"
       />
+      <Script id="track-ga" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-QNLF4HTK6S');
+      `}</Script>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QNLF4HTK6S"
+        strategy="afterInteractive"
+      />
     </>
   );
 }
