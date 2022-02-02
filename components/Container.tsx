aad7a5eb992fef30f27795be763cdd811109b389
Fatih Kalifa <fatihkalifa@gmail.com>
2022-02-02 10:47:45 +0700

    fix layout on iphone

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function Container(props: PropsWithChildren<{}>) {
   return (
-    <div className="h-screen text-white text-center flex flex-col items-stretch overflow-y-hidden">
+    <div className="text-white text-center flex flex-col items-stretch overflow-y-hidden">
       {props.children}
     </div>
   );
