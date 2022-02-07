03c9d974bdcb7b088a7d13d579df0be54538b4a7
Fatih Kalifa <fatihkalifa@gmail.com>
2022-02-07 13:28:17 +0700

    fix light mode default text color

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function Container(props: PropsWithChildren<{}>) {
   return (
-    <div className="text-white text-center flex flex-col items-stretch overflow-y-hidden">
+    <div className="text-gray-800 dark:text-white text-center flex flex-col items-stretch overflow-y-hidden">
       {props.children}
     </div>
   );
