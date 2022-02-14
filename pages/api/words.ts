688a27a1b7fe1ccc1416106814c3276e8b3f5dc1
Fatih Kalifa <fatihkalifa@gmail.com>
2022-02-14 10:38:10 +0700

    october 2021

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -9,11 +10,90 @@ export default async function handler(
       const [word] = entry.split("/").reverse();
       return word;
     })
-    .filter((word) => /^[a-z]+$/.test(word) && word.length === 5);
+    .filter((word) => /^[a-z]+$/.test(word) && word.length === 5)
+    .concat(october2021)
+    .sort();
 
   res.setHeader(
     "Cache-Control",
     "public, s-maxage=3600, stale-while-revalidate=86400"
   );
-  res.status(200).json(words);
+  res.status(200).json(Array.from(new Set(words)));
 }
+
+const october2021 = [
+  "abate",
+  "abjat",
+  "ampet",
+  "arame",
+  "asrot",
+  "azeri",
+  "azuki",
+  "bakes",
+  "benin",
+  "beton",
+  "bezit",
+  "bksda",
+  "botia",
+  "burma",
+  "cekah",
+  "cekat",
+  "cenah",
+  "colon",
+  "dapuk",
+  "datau",
+  "datin",
+  "datin",
+  "datum",
+  "denar",
+  "dobra",
+  "doula",
+  "duvet",
+  "filem",
+  "folio",
+  "gabut",
+  "gayor",
+  "gokil",
+  "gravi",
+  "hipmi",
+  "ilyas",
+  "impun",
+  "india",
+  "islan",
+  "kabul",
+  "kalke",
+  "kazak",
+  "korea",
+  "kudet",
+  "kurdi",
+  "kwaca",
+  "lakip",
+  "mandu",
+  "maori",
+  "nuget",
+  "ompok",
+  "palau",
+  "panda",
+  "pasto",
+  "porus",
+  "rasis",
+  "rouks",
+  "rupst",
+  "sango",
+  "sapun",
+  "silpa",
+  "sonde",
+  "struk",
+  "sudan",
+  "swazi",
+  "tafia",
+  "tajik",
+  "tando",
+  "tenge",
+  "titis",
+  "tonga",
+  "tuhur",
+  "uwete",
+  "uzbek",
+  "venda",
+];
