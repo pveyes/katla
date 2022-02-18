9683e61559b6167d6b4e08864a8eed9b96ba40c6
Fatih Kalifa <fatihkalifa@gmail.com>
2022-02-18 13:55:06 +0700

    add explicit lang and translate attribute

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="id" translate="no">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
