f6be9fa7d6ade345ea11512d84830793ca83db88
M Gilang Januar <mgilangjanuar@gmail.com>
2022-05-06 10:47:17 +0700

    fix concate string in share text

--- components/StatsModal.tsx
+++ components/StatsModal.tsx
@@ -91,11 +91,9 @@ export default function StatsModal(props: Props) {
       return text;
     }
 
-    text +=
-      "\nDurasi: " +
-      `${stats.duration.hours}:${pad0(stats.duration.minutes)}:${pad0(
-        stats.duration.seconds
-      )}`;
+    text += `\nDurasi: ${stats.duration.hours}:${pad0(
+      stats.duration.minutes
+    )}:${pad0(stats.duration.seconds)}`;
     text += "\n" + window.location.href;
     return text;
   }
@@ -151,10 +149,9 @@ export default function StatsModal(props: Props) {
     ctx.fillText(text, canvas.width / 2, 300);
     ctx.font = "32px sans-serif";
     ctx.fillText(
-      "Durasi: " +
-        `${stats.duration.hours}:${pad0(stats.duration.minutes)}:${pad0(
-          stats.duration.seconds
-        )}`,
+      `Durasi: ${stats.duration.hours}:${pad0(stats.duration.minutes)}:${pad0(
+        stats.duration.seconds
+      )}`,
       canvas.width / 2,
       canvas.height - 300
     );
