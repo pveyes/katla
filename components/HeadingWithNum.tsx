4291e6d06456554407f59a63976aaebe7ed5f96c
Fatih Kalifa <fatihkalifa@gmail.com>
2023-08-17 12:17:52 +0700

    custom red-white

--- components/HeadingWithNum.tsx
+++ components/HeadingWithNum.tsx
@@ -4,11 +4,21 @@ interface Props {
 }
 
 export default function HeadingWithNum(props: Props) {
+  const [_, mm, dd] = new Date().toISOString().split("T")[0].split("-");
+  const isIndonesiaIndependenceDay = mm === "08" && dd === "17";
+  let customNumClass = "";
+  if (isIndonesiaIndependenceDay) {
+    customNumClass = "text-white bg-red-500 p-1";
+  }
+
   return (
     <span>
       {props.enableLiarMode ? "Katlie" : "Katla"}
       {props.num && (
-        <sup className="-top-4 tracking-tight" style={{ fontSize: "45%" }}>
+        <sup
+          className={`-top-4 tracking-tight ${customNumClass}`}
+          style={{ fontSize: "45%" }}
+        >
           #{props.num}
         </sup>
       )}
