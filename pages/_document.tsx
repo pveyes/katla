fe1f05dd81500bad61e5a70e8a57cdb1d733722c
Fatih Kalifa <fatihkalifa@gmail.com>
2023-10-23 20:15:52 +0700

    ads meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="id" translate="no">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-3081263972680635" />
+      </Head>
       <body>
         <Main />
         <NextScript />
